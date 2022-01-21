dc5b85dd923f1744fcdd570bb1f17f32d98c7132
Haro <ebuovchu@gmail.com>
2022-01-21 21:06:25 +0300

    Add new GameScene

--- assets/scripts/scenes/StartScene.js
+++ assets/scripts/scenes/StartScene.js
@@ -4,8 +4,22 @@ class StartScene extends Phaser.Scene {
     }
     create() {
         this.createBackground();
+        this.createText();
+        this.setEvents();
     }
     createBackground() {
         this.add.sprite(0, 0, 'bg').setOrigin(0);
+
+    }
+    createText() {
+        this.add.text(config.width / 2, 500, 'Tap to start', {
+            font: '40px CurseCasual',
+            fill: '#FFFFFF'
+        }).setOrigin(0.5);
+    }
+    setEvents() {
+        this.input.on('pointerdown', () => {
+            this.scene.start('Game');
+        })
     }
 }
